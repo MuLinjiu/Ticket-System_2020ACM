7ea7ce36a5f75c3cfcd0afb45877199928759967
Lhtie <lhxlhy2009@sina.com>
2021-04-30 13:00:10 +0800

    Enable insert & search

--- include/BpTree.hpp
+++ include/BpTree.hpp
@@ -30,6 +30,32 @@ private:
 	Storage_IO<Value> data_file;
 	fstream bpt_basic_file;
 
+	int split(int child, node &y){
+		int l = y.num_keys / 2, r = y.num_keys - l;
+		node z;
+		int new_child = bpt_node_file.write(z);
+		y.num_keys = l, z.num_keys = r;
+		if (y.is_leaf){
+			z.is_leaf = true;
+			z.succ = y.succ, y.succ = new_child, z.prec = child;
+			if (y.succ){
+				node tmp; bpt_node_file.read(y.succ, tmp);
+				tmp.prec = new_child;
+				bpt_node_file.write(y.succ, tmp);
+			}
+			for (int i = 0; i < r; ++i)
+				z.keys[i] = y.keys[l+i], z.birec[i].index[i] = y.birec[l+i].index;
+		} else {
+			r--, z.num_keys--;
+			for (int i = 0; i < r; ++i)
+				z.keys[i] = y.keys[l+i+1];
+			for (int i = 0; i <= r; ++i)
+				z.birec[i].child = y.birec[l+i+1].child;
+		}
+		bpt_node_file.write(child, y), bpt_node_file(new_child, z);
+		return new_child;
+	}
+
 	void insert(int current, const Key &key, int index){ // current-x, child-y, new_child-z
 		node x, y;
 		int ptr = 0, child;
@@ -48,32 +74,25 @@ private:
 		insert(child, key, index);
 		bpt_node_file.read(child, y);
 		if (y.is_leaf && y.num_keys > M || !y.is_leaf && y.num_keys >= M){
-			int l = y.num_keys / 2, r = y.num_keys - l;
-			node z;
-			int new_child = bpt_node_file.write(z);
-			y.num_keys = l, z.num_keys = r;
-			if (y.is_leaf){
-				z.is_leaf = true;
-				z.succ = y.succ, y.succ = new_child, z.prec = child;
-				for (int i = 0; i < r; ++i)
-					z.keys[i] = y.keys[l+i], z.birec[i].index[i] = y.birec[l+i].index;
-			} else {
-				r--, z.num_keys--;
-				for (int i = 0; i < r; ++i)
-					z.keys[i] = y.keys[l+i+1];
-				for (int i = 0; i <= r; ++i)
-					z.birec[i].child = y.birec[l+i+1].child;
-			}
-			bpt_node_file.write(child, y), bpt_node_file(new_child, z);
+			int new_child = split(child, y);
 			for (int i = x.num_keys; i > ptr; i--)
 				x.keys[i] = x.keys[i-1];
 			for (int i = x.num_keys + 1; i > ptr; i--)
 				x.birec[i].index = x.birec[i-1].index;
 			x.num_keys++;
-			x.keys[ptr] = y.keys[l];
+			x.keys[ptr] = y.keys[y.num_keys];
 			x.birec[ptr].child = child, x.birec[ptr+1].child = new_child;
 			bpt_node_file.write(current, x);
 		}
+	}
+
+	node search(int current, const Key &key){
+		node x;
+		int ptr = 0;
+		bpt_node_file.read(current, x);
+		if (x.is_leaf) return x;
+		for (; ptr < x.num_keys && x.keys[ptr] <= key; ++ptr);
+		return search(x.birec[ptr].child, key);
 	}
 
 public:
@@ -96,6 +115,43 @@ public:
 	}
 
 	~BpTree(){ bpt_basic_file.close(); }
+
+	void insert(const Key &key, const Value &val){
+		int index = data_file.write(val);
+		insert(root, key, index);
+		node x;
+		bpt_node_file(root, x);
+		if (x.is_leaf && x.num_keys > M || !x.is_leaf && x.num_keys >= M){
+			int new_child = split(root, x);
+			node y;
+			int new_root = bpt_node_file.write(y);
+			y.num_keys = 1;
+			y.keys[0] = x.keys[x.num_keys];
+			y.birec[0].child = root, y.birec[1].child = new_child;
+			root = new_root;
+			bpt_node_file(root, y);
+			bpt_basic_file.seekp(0);
+			bpt_basic_file.write(reinterpret_cast<char *> (&root), sizeof(int));
+		}
+	}
+
+	vector<Value> search(const Key &key, bool (*equ)(Key, Key)){
+		node x = search(root, key);
+		int ptr = 0;
+		for (; ptr < x.num_keys && x.keys[ptr] < key; ++ptr);
+		vector<Value> ret;
+		while(equ(x.keys[ptr], key)){
+			Value val;
+			data_file(x.birec[ptr].index, val);
+			ret.push_back(val);
+			if (++ptr >= x.num_keys){
+				if (!x.succ) break;
+				bpt_node_file.read(x.succ, x);
+				ptr = 0;
+			}
+		}
+		return ret;
+	}
 
 };
 
